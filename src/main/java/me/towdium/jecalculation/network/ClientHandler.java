62eef4ffd2d9179d2bf29aa0f688ab9974fc89df
Discreater <Discreater@163.com>
2021-06-21 03:00:09 +0800

    fix: remove command

--- src/main/java/me/towdium/jecalculation/network/ClientHandler.java
+++ src/main/java/me/towdium/jecalculation/network/ClientHandler.java
@@ -1,15 +1,16 @@
 package me.towdium.jecalculation.network;
 
 import cpw.mods.fml.client.registry.ClientRegistry;
-import me.towdium.jecalculation.command.JecaCommand;
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
 import me.towdium.jecalculation.data.Controller;
 import me.towdium.jecalculation.data.label.ILabel;
 import me.towdium.jecalculation.event.Handlers;
-import net.minecraftforge.client.ClientCommandHandler;
 
 import static me.towdium.jecalculation.gui.JecaGui.keyOpenGuiCraft;
 import static me.towdium.jecalculation.gui.JecaGui.keyOpenGuiMath;
 
+@SideOnly(Side.CLIENT)
 public class ClientHandler {
 
     public void initPre() {
@@ -18,7 +19,8 @@ public class ClientHandler {
 
     public void initPost() {
         ILabel.initServer();
-        ClientCommandHandler.instance.registerCommand(new JecaCommand());
+        // May crash: `NoSuchMethodError`.
+        //        ClientCommandHandler.instance.registerCommand(new JecaCommand());
         ILabel.initClient();
         Controller.loadFromLocal();
     }
